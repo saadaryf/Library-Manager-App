bc83c6d47e858bbecdbb5e8183a287a5161a4d58
Saad <saadxdhillon@gmail.com>
2023-09-11 15:20:52 +0500

    git commit -m "update"

--- src/main/java/com/library/libraryManager/controller/HomeController.java
+++ src/main/java/com/library/libraryManager/controller/HomeController.java
@@ -1,10 +1,32 @@
 package com.library.libraryManager.controller;
 
+import com.library.libraryManager.mapper.AuthorMapper;
+import com.library.libraryManager.mapper.BooksMapper;
+import com.library.libraryManager.model.Author;
+import com.library.libraryManager.model.Books;
+import com.library.libraryManager.response.AuthorResponse;
+import com.library.libraryManager.response.BooksResponse;
+import com.library.libraryManager.services.AuthorService;
+import com.library.libraryManager.services.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
+    @Autowired
+    AuthorService authorService;
+    @Autowired
+    BooksService booksService;
+    @Autowired
+    AuthorMapper authorMapper;
+    @Autowired
+    BooksMapper booksMapper;
+
     @GetMapping("/")
     public String showMainPage(){
         return "mainPage";
@@ -35,4 +57,19 @@ public class HomeController {
     public String updateBook(){
         return "/Books/update";
     }
+
+    @GetMapping("/viewAll")
+    public String viewAuthor(Model model){
+        List<Author> authorList=authorService.getAuthors();
+        List<AuthorResponse> authorResponses= authorList.stream()
+                .map(author->authorMapper.mapToDto(author))
+                .collect(Collectors.toList());
+        model.addAttribute("authorResponses",authorResponses);
+        return "/viewAll";
+    }
+   /* @GetMapping("viewAll")
+    public String viewAll(Model model){
+        List<AuthorResponse> authorResponses=
+        return "/viewAll";
+    }*/
 }
